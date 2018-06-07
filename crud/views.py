39b5a535f84e943e6cd4e5e32fa895ec868d5d72
Victor Leal <victorleal18@gmail.com>
2018-06-06 22:20:32 -0300

    Initial commit with everything before tests

---
+++ crud/views.py
@@ -0,0 +1,41 @@
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
+from .models import Question, Choice
+
+# Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'crud/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'crud/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'crud/results.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'crud/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('crud:results', args=(question.id,)))
